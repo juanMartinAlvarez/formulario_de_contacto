904ec12b32847d37e786585aa098092b081f61df
martin alvarez <juan_martin_alvarez@yahoo.com.ar>
2020-05-27 00:55:08 -0300

    Create enviar2.php
    Add the Php file that will send the form to the teacher's email

---
+++ enviar2.php
@@ -0,0 +1,23 @@
+<?php
+$nombre = $_POST['nombre'];
+$apellido = $_POST['apellido']
+$mail = $_POST['email'];
+$asunto = $_POST['asunto'];
+$empresa = $_POST['mensaje'];
+
+$header = 'From: ' . $mail . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
+$mensaje .= "Su e-mail es: " . $mail . " \r\n";
+$mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
+$mensaje .= "Enviado el " . date('d/m/Y', time());
+
+$para = 'mcferrocroce@gmail.com';
+
+mail($para, $asunto, utf8_decode($mensaje), $header);
+
+header("Location:index.html");
+?>
